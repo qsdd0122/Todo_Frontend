68decb9b47a6453a2a9aed62bbee8db277393529
actic1014 <96825988+qsdd0122@users.noreply.github.com>
2022-01-12 21:38:57 +0900

    백엔드 서비스 통합

---
+++ src/Todo.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { ListItem, ListItemText, InputBase, Checkbox
+    ,ListItemSecondaryAction, IconButton} from "@material-ui/core";
+import { DeleteOutlined } from '@material-ui/icons';
+
+class Todo extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {item: props.item, readOnly:true};
+    this.delete = props.delete;
+    this.update = props.update;
+  }
+
+  deleteEventHandler = () => {
+      this.delete(this.state.item);
+  }
+
+  offReadOnlyMode = () => {
+      console.log("Event!",  this.state.readOnly)
+      this.setState({ readOnly: false}, () => {
+          console.log("ReadOnly?", this.state.readOnly)
+      });
+  }
+
+  enterKeyEventHandler = (e) => {
+      if(e.key === "Enter") {
+          this.setState({ readOnly: true});
+          this.update(this.state.item); //엔터를 누르면 저장
+      }
+  }
+
+  editEventHandle = (e) => {
+      const thisItem = this.state.item;
+      thisItem.title = e.target.value;
+      this.setState({item: thisItem});
+  }
+
+  checkboxEventHandler = (e) => {
+      const thisItem = this.state.item;
+      thisItem.done = !thisItem.done;
+      this.setState({item:thisItem});
+      this.update(this.state.item); // 체크박스가 변경되면 저장
+  }
+
+  render(){
+    const item = this.state.item;
+    return (
+        <ListItem>
+            <Checkbox checked={item.done} onChange={this.checkboxEventHandler}/>
+            <ListItemText>
+                <InputBase
+                    inputProps={{"aria-label":"naked",
+                        readOnly: this.state.readOnly}}
+                    onClick={this.offReadOnlyMode}
+                    onKeyPress={this.enterKeyEventHandler}
+                    onChange={this.editEventHandle}
+                    type="text"
+                    id={item.id}
+                    name={item.id}
+                    value={item.title}
+                    multiline={true}
+                    fullWidth={true}
+                />
+            </ListItemText>
+            
+            <ListItemSecondaryAction>
+                <IconButton aria-label='Delete Todo'
+                    onClick={this.deleteEventHandler}>
+                    <DeleteOutlined/>
+                </IconButton>
+            </ListItemSecondaryAction>
+        </ListItem>
+    );
+  }
+}
+
+export default Todo;
