08f845bec7c6f4e64f0ad13896ecbf97f25340a5
actic1014 <96825988+qsdd0122@users.noreply.github.com>
2022-01-16 22:45:48 +0900

    App컴포넌트 수정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Todo from "./Todo";
+import AddTodo from "./AddTodo";
+import { Paper, List, Container } from "@material-ui/core";
+import { call } from "./service/ApiService";
+ 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [ ]
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  componentDidMount() {
+    call("/todo", "GET", null).then((response) =>
+      this.setState({ items: response.data })
+    );
+  }
+
+  add = (item) => {
+    call("/todo", "POST", null).then((response) =>
+      this.setState({ items: response.data })
+    );
+  }
+
+  delete = (item) => {
+    call("/todo", "DELETE", null).then((response) =>
+      this.setState({ items: response.data })
+    );
+  }
+
+  update = (item) => {
+    call("/todo", "PUT", null).then((response) =>
+      this.setState({ items: response.data })
+    );
+  }
+
+  render() {
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin:16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item = {item} key = {item.id} delete={this.delete} />
+          ))}
+        </List>
+      </Paper>
+    );
+    
+    return (
+    <div className='App'>
+      <Container maxWidth="md">
+        <AddTodo add = {this.add}/>
+        <div className="TodoList">{todoItems}</div>
+      </Container>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
