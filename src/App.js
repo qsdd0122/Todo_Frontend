c9b4a16d6a0eea23218cf0cff30b5e9beb60fdbb
actic1014 <96825988+qsdd0122@users.noreply.github.com>
2022-01-17 21:41:14 +0900

    로딩 페이지
    UI 글리치 해결

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [ ]
+      items: [],
+      loading: true // 로딩 중이라는 상태를 표현할 변수
     };
   }
 
   componentDidMount() {
+    // GET 요청이 성공적으로 리턴하는 경우 loading을 false로 고친다.
     call("/todo", "GET", null).then((response) =>
-      this.setState({ items: response.data })
+      this.setState({ items: response.data, loading: false })
     );
   }
 
@@ -66,9 +68,9 @@ class App extends React.Component {
       </AppBar>
     );
     
-    //props로 넘겨주기
-    return (
-    <div className='App'>
+    // 로딩 중이 아닐 때 렌더링할 부분
+    var todoListPage =  (
+    <div>
       {navigationBar} {/* 네비게이션 바 렌더링 */}
       <Container maxWidth="md">
         <AddTodo add = {this.add}/>
@@ -76,6 +78,18 @@ class App extends React.Component {
       </Container>
     </div>
     );
+
+    // 로딩 중일 때 렌더링할 부분
+    var loadingPage = <h1> 로딩 중... </h1>;
+    var content = loadingPage;
+
+     // 로딩중이 아니라면 todoListPage를 선택
+    if(!this.state.loading) {
+      content = todoListPage;
+    }
+
+    // 선택한 content 렌더링
+    return <div className="App">{content}</div>
   }
 }
 
